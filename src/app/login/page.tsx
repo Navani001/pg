e551841e90175423c31f0e1100dbad4f9532615d
Navani001 <navaneetha2006krishnan@gmail.com>
2025-09-14 17:41:48 +0530

    login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,8 +3,9 @@
 import { Login } from "@/component";
 export default function LoginPage() {
     return (
-        <div className="min-h-screen bg-slate-100 flex items-center justify-center p-4 font-inter">
+        <div className="min-h-screen bg-[url(/loginBgImage.png)] w-full bg-cover flex items-center justify-center p-4 font-inter">
             <Login/>
+            {/* <InputField/> */}
         </div>
     );
 }
