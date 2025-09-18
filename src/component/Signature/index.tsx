214dc9eb568584b3616c9f393699580f0f2c3bf7
navani_hk <navaneetha2006krishnan@gmail.com>
2025-09-18 13:44:21 +0530

    proof page

--- src/component/Signature/index.tsx
+++ src/component/Signature/index.tsx
@@ -6,6 +6,7 @@ import { FaUndoAlt } from "react-icons/fa";
 import { IoPencil } from "react-icons/io5";
 import { LucideUndo } from "lucide-react";
 import { MdUndo } from "react-icons/md";
+import { cn } from "@heroui/react";
 
 export const SignaturePad: React.FC = () => {
     const sigCanvas = useRef<SignatureCanvas>(null);
@@ -21,46 +22,47 @@ export const SignaturePad: React.FC = () => {
     };
 
     return (
-        <div className=" flex flex-col p-3 border rounded-xl gap-3">
+        <div className="w-full flex flex-col p-3 border rounded-xl gap-3">
             <h4 className="font-semibold text-lg">Digital Signature</h4>
-        <div className="flex flex-col  space-y-4">
-            <SignatureCanvas
-                ref={sigCanvas}
-                penColor={isDrawing ? "black" : "transparent"}
-                canvasProps={{
-                    className: "border-1  rounded-lg w-full h-full"
-                }}
-            />
-
-            <div className="space-x-4">
-                <ButtonComponent handleOnClick={clear}
-                    buttonText="Clear"
-                    textClassName="font-normal "
-                        baseClassName="w-auto h-auto p-2  border border-black"
-                    buttonIcon={<FaUndoAlt />} isStartIcon isEndIcon={false} isIcon={true} />
-                <ButtonComponent handleOnClick={() => {
-                    setIsDrawing(!isDrawing);
-                }}
-                    buttonText="Draw"
-                    textClassName="font-normal "
-                        baseClassName="w-auto h-auto p-2 border border-black"
-                    buttonIcon={<IoPencil className="text-md" />} isStartIcon isEndIcon={false} isIcon={true} />
-
-                <ButtonComponent
-                    handleOnClick={() => {
-                        const data = sigCanvas.current?.toData();
-                        if (data && data.length > 0) {
-                            data.pop(); // remove last stroke
-                            sigCanvas.current?.fromData(data); // redraw remaining
-                        }
+            <div className="flex w-full flex-col  space-y-4">
+                <SignatureCanvas
+                    ref={sigCanvas}
+                    penColor={isDrawing ? "black" : "transparent"}
+                    canvasProps={{
+                        className: "border-1  rounded-lg w-full h-full"
                     }}
-                    buttonText="Undo"
-                    textClassName="font-normal "
-                    baseClassName="w-auto h-auto p-2 border border-black"
-                    buttonIcon={<MdUndo />} isStartIcon isEndIcon={false} isIcon={true} />
+                />
 
+                <div className="space-x-4">
+                    <ButtonComponent handleOnClick={clear}
+                        buttonText="Clear"
+                        textClassName="font-normal "
+                        baseClassName="w-auto h-auto p-2  border border-black"
+                        buttonIcon={<FaUndoAlt />} isStartIcon isEndIcon={false} isIcon={true} />
+                    <ButtonComponent handleOnClick={() => {
+                        setIsDrawing(!isDrawing);
+                    }}
+                        buttonText="Draw"
+                        textClassName="font-normal "
+                        bgColor={isDrawing ? "bg-primary" : "bg-transparent"}
+                        baseClassName={cn("w-auto h-auto p-2 border border-black", { "bg-primary  border-0 text-white": isDrawing })}
+                        buttonIcon={<IoPencil className="text-md" />} isStartIcon isEndIcon={false} isIcon={true} />
+
+                    <ButtonComponent
+                        handleOnClick={() => {
+                            const data = sigCanvas.current?.toData();
+                            if (data && data.length > 0) {
+                                data.pop(); // remove last stroke
+                                sigCanvas.current?.fromData(data); // redraw remaining
+                            }
+                        }}
+                        buttonText="Undo"
+                        textClassName="font-normal "
+                        baseClassName="w-auto h-auto p-2 border border-black"
+                        buttonIcon={<MdUndo />} isStartIcon isEndIcon={false} isIcon={true} />
+
+                </div>
             </div>
-        </div>
         </div>
     );
 };
