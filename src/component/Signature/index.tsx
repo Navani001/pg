e84c4b7f07e09b3e8225a41a7bff3746cbe91822
navani_hk <navaneetha2006krishnan@gmail.com>
2025-09-18 12:47:26 +0530

    test

--- src/component/Signature/index.tsx
+++ src/component/Signature/index.tsx
@@ -29,7 +29,7 @@ export const SignaturePad: React.FC = () => {
                 <button onClick={clear} className="px-4 py-2 bg-gray-300 rounded">
                     Clear
                 </button>
-                  <ButtonComponent handleOnClick={save} isStartIcon buttonIcon={} isEndIcon={false} isIcon={true} />
+                  <ButtonComponent handleOnClick={save} isStartIcon isEndIcon={false} isIcon={true} />
                 {/* <button onClick={save} className="px-4 py-2 bg-blue-500 text-white rounded">
                     Save
                 </button> */}
